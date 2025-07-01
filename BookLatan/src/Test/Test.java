66daf1974ab9670c6c29aae73be0ced2e1ddc99d
Dinel <Dinel@Dinel>
2025-07-01 12:24:39 +0800

    JULY 1PA RIN

--- BookLatan/src/Test/Test.java
+++ BookLatan/src/Test/Test.java
@@ -11,8 +11,5 @@ package Test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("TANG World!");
-        System.out.println("Hello INA!");
-        
     }
 }
