52f575bcba82e1adf4f66a51ec7b5a88fcf62bbf
KaiserLycan <josephreyramirezpanti+github@gmail.com>
2025-07-07 12:24:40 +0800

    Application adjust to screen scaling.

--- BookLatan/src/Main/Application.java
+++ BookLatan/src/Main/Application.java
@@ -14,15 +14,13 @@ public abstract class Application extends JFrame {
     protected ArrayList<JButton> btns = new ArrayList<>();
     
     public Application() {
-        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        GraphicsDevice gd = ge.getDefaultScreenDevice();
-        DisplayMode mode = gd.getDisplayMode();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         
         this.setTitle("BookLatan");
         this.setName("Staff Application");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setIconImage(new ImageIcon("./src/Images/userimage.png").getImage());
-        this.setPreferredSize(new Dimension(mode.getWidth(), mode.getHeight() - 20));
+        this.setPreferredSize(new Dimension(screenSize.width, screenSize.height - 20));
         this.setSize(this.getPreferredSize());
         this.setLocationRelativeTo(null);
         this.getContentPane().setBackground(new Color(245, 245, 245));
