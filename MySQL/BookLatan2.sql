e548ca63c217243cdc427f036d9c928f4bf0814f
KaiserLycan <josephreyramirezpanti+github@gmail.com>
2025-07-07 12:10:24 +0800

    New DB

---
+++ MySQL/BookLatan2.sql
@@ -0,0 +1,462 @@
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: localhost    Database: booklatan
+-- ------------------------------------------------------
+-- Server version	8.0.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `author`
+--
+
+DROP TABLE IF EXISTS `author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `author` (
+  `authorID` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(500) NOT NULL,
+  `bio` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`authorID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `author`
+--
+
+LOCK TABLES `author` WRITE;
+/*!40000 ALTER TABLE `author` DISABLE KEYS */;
+/*!40000 ALTER TABLE `author` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `book` (
+  `bookID` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(500) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `pubDate` date NOT NULL,
+  `lang` varchar(255) NOT NULL,
+  `_status` enum('loaned','available','not available','reserved') DEFAULT 'not available',
+  `shelfLocation` varchar(255) DEFAULT NULL,
+  `pubID` int NOT NULL,
+  `libID` int NOT NULL,
+  PRIMARY KEY (`bookID`),
+  KEY `pubID` (`pubID`),
+  KEY `libID` (`libID`),
+  CONSTRAINT `book_ibfk_1` FOREIGN KEY (`pubID`) REFERENCES `publisher` (`pubID`),
+  CONSTRAINT `book_ibfk_2` FOREIGN KEY (`libID`) REFERENCES `library` (`libID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book`
+--
+
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bookauthor`
+--
+
+DROP TABLE IF EXISTS `bookauthor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bookauthor` (
+  `bookID` int NOT NULL,
+  `authorID` int NOT NULL,
+  PRIMARY KEY (`bookID`,`authorID`),
+  KEY `authorID` (`authorID`),
+  CONSTRAINT `bookauthor_ibfk_1` FOREIGN KEY (`bookID`) REFERENCES `book` (`bookID`),
+  CONSTRAINT `bookauthor_ibfk_2` FOREIGN KEY (`authorID`) REFERENCES `author` (`authorID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bookauthor`
+--
+
+LOCK TABLES `bookauthor` WRITE;
+/*!40000 ALTER TABLE `bookauthor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bookauthor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fine`
+--
+
+DROP TABLE IF EXISTS `fine`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fine` (
+  `fineID` int NOT NULL AUTO_INCREMENT,
+  `staffID` int NOT NULL,
+  `memberID` int NOT NULL,
+  `amount` decimal(50,2) NOT NULL,
+  `reason` varchar(500) NOT NULL,
+  `dateIssued` date NOT NULL,
+  `_status` enum('paid','unpaid') DEFAULT 'unpaid',
+  PRIMARY KEY (`fineID`),
+  KEY `memberID` (`memberID`),
+  KEY `staffID` (`staffID`),
+  CONSTRAINT `fine_ibfk_1` FOREIGN KEY (`memberID`) REFERENCES `member` (`memberID`),
+  CONSTRAINT `fine_ibfk_2` FOREIGN KEY (`staffID`) REFERENCES `staff` (`staffID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fine`
+--
+
+LOCK TABLES `fine` WRITE;
+/*!40000 ALTER TABLE `fine` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fine` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `library`
+--
+
+DROP TABLE IF EXISTS `library`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `library` (
+  `libID` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  PRIMARY KEY (`libID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `library`
+--
+
+LOCK TABLES `library` WRITE;
+/*!40000 ALTER TABLE `library` DISABLE KEYS */;
+/*!40000 ALTER TABLE `library` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `loan`
+--
+
+DROP TABLE IF EXISTS `loan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `loan` (
+  `loanID` int NOT NULL AUTO_INCREMENT,
+  `issueDate` date NOT NULL,
+  `dueDate` date NOT NULL,
+  `returnDate` date DEFAULT NULL,
+  `memberID` int NOT NULL,
+  PRIMARY KEY (`loanID`),
+  KEY `memberID` (`memberID`),
+  CONSTRAINT `loan_ibfk_1` FOREIGN KEY (`memberID`) REFERENCES `member` (`memberID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `loan`
+--
+
+LOCK TABLES `loan` WRITE;
+/*!40000 ALTER TABLE `loan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `loan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `loanbook`
+--
+
+DROP TABLE IF EXISTS `loanbook`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `loanbook` (
+  `loanID` int NOT NULL,
+  `bookID` int NOT NULL,
+  PRIMARY KEY (`loanID`,`bookID`),
+  KEY `bookID` (`bookID`),
+  CONSTRAINT `loanbook_ibfk_1` FOREIGN KEY (`bookID`) REFERENCES `book` (`bookID`),
+  CONSTRAINT `loanbook_ibfk_2` FOREIGN KEY (`loanID`) REFERENCES `loan` (`loanID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `loanbook`
+--
+
+LOCK TABLES `loanbook` WRITE;
+/*!40000 ALTER TABLE `loanbook` DISABLE KEYS */;
+/*!40000 ALTER TABLE `loanbook` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `member`
+--
+
+DROP TABLE IF EXISTS `member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `member` (
+  `memberID` int NOT NULL AUTO_INCREMENT,
+  `userID` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `dateJoined` date NOT NULL,
+  `_status` enum('active','suspended','deactivated') DEFAULT 'active',
+  PRIMARY KEY (`memberID`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `uq_userID` (`userID`),
+  CONSTRAINT `member_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `member`
+--
+
+LOCK TABLES `member` WRITE;
+/*!40000 ALTER TABLE `member` DISABLE KEYS */;
+INSERT INTO `member` VALUES (3,3,'Caily Lycan','203-2034-0234','kaiser@gmail.com','Caloocan, Philippines','2025-07-01','suspended'),(4,4,'Dinel Palencia','123-123-123','dcpr123@gmail.com','Taguig, Phlippines','2025-07-02','suspended'),(9,8,'Khryzna Advincula','123-123-123','khryz@gmail.com','Manila, Philippines','2025-07-07','active');
+/*!40000 ALTER TABLE `member` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary view structure for view `memberuser`
+--
+
+DROP TABLE IF EXISTS `memberuser`;
+/*!50001 DROP VIEW IF EXISTS `memberuser`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `memberuser` AS SELECT 
+ 1 AS `userID`,
+ 1 AS `memberID`,
+ 1 AS `name`,
+ 1 AS `phone`,
+ 1 AS `email`,
+ 1 AS `address`,
+ 1 AS `dateJoined`,
+ 1 AS `_status`,
+ 1 AS `userName`,
+ 1 AS `password`,
+ 1 AS `type`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `payments`
+--
+
+DROP TABLE IF EXISTS `payments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `payments` (
+  `paymentID` int NOT NULL AUTO_INCREMENT,
+  `fineID` int NOT NULL,
+  `amountPaid` decimal(50,2) NOT NULL DEFAULT '0.00',
+  `paymentChange` decimal(50,2) NOT NULL DEFAULT '0.00',
+  `datePaid` date DEFAULT NULL,
+  PRIMARY KEY (`paymentID`),
+  KEY `fineID` (`fineID`),
+  CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`fineID`) REFERENCES `fine` (`fineID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `payments`
+--
+
+LOCK TABLES `payments` WRITE;
+/*!40000 ALTER TABLE `payments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `payments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `publisher`
+--
+
+DROP TABLE IF EXISTS `publisher`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `publisher` (
+  `pubID` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(500) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`pubID`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `publisher`
+--
+
+LOCK TABLES `publisher` WRITE;
+/*!40000 ALTER TABLE `publisher` DISABLE KEYS */;
+/*!40000 ALTER TABLE `publisher` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reservation`
+--
+
+DROP TABLE IF EXISTS `reservation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservation` (
+  `resID` int NOT NULL AUTO_INCREMENT,
+  `memberID` int NOT NULL,
+  `dateReserved` date NOT NULL,
+  `_status` enum('canceled','claimed') DEFAULT NULL,
+  PRIMARY KEY (`resID`),
+  KEY `memberID` (`memberID`),
+  CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`memberID`) REFERENCES `member` (`memberID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservation`
+--
+
+LOCK TABLES `reservation` WRITE;
+/*!40000 ALTER TABLE `reservation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reservation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reservationbook`
+--
+
+DROP TABLE IF EXISTS `reservationbook`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservationbook` (
+  `resID` int NOT NULL,
+  `bookID` int NOT NULL,
+  PRIMARY KEY (`resID`,`bookID`),
+  KEY `bookID` (`bookID`),
+  CONSTRAINT `reservationbook_ibfk_1` FOREIGN KEY (`bookID`) REFERENCES `book` (`bookID`),
+  CONSTRAINT `reservationbook_ibfk_2` FOREIGN KEY (`resID`) REFERENCES `reservation` (`resID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservationbook`
+--
+
+LOCK TABLES `reservationbook` WRITE;
+/*!40000 ALTER TABLE `reservationbook` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reservationbook` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `staff`
+--
+
+DROP TABLE IF EXISTS `staff`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `staff` (
+  `staffID` int NOT NULL AUTO_INCREMENT,
+  `userID` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `dateHired` date NOT NULL,
+  `role` enum('admin','librarian') NOT NULL,
+  PRIMARY KEY (`staffID`),
+  UNIQUE KEY `email` (`email`),
+  KEY `userID` (`userID`),
+  CONSTRAINT `staff_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `staff`
+--
+
+LOCK TABLES `staff` WRITE;
+/*!40000 ALTER TABLE `staff` DISABLE KEYS */;
+/*!40000 ALTER TABLE `staff` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `userID` int NOT NULL AUTO_INCREMENT,
+  `userName` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL DEFAULT 'user1234',
+  `type` enum('member','admin','librarian') NOT NULL,
+  PRIMARY KEY (`userID`),
+  UNIQUE KEY `userName` (`userName`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'kai1','kai1234','admin'),(2,'kai2','kai1234','librarian'),(3,'kai3','kai1234','member'),(4,'dcpr','1234','member'),(8,'Khryz','1234','member');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Final view structure for view `memberuser`
+--
+
+/*!50001 DROP VIEW IF EXISTS `memberuser`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `memberuser` AS select `member`.`userID` AS `userID`,`member`.`memberID` AS `memberID`,`member`.`name` AS `name`,`member`.`phone` AS `phone`,`member`.`email` AS `email`,`member`.`address` AS `address`,`member`.`dateJoined` AS `dateJoined`,`member`.`_status` AS `_status`,`users`.`userName` AS `userName`,`users`.`password` AS `password`,`users`.`type` AS `type` from (`member` join `users` on((`member`.`userID` = `users`.`userID`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-07-07 12:10:05
