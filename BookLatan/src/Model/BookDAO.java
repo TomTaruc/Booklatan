88dde1d4bd3ad9dbdf0e0564d3d518abfdc23e27
Lenovo <Lenovo@LAPTOP-NE9IU6FK>
2025-07-26 21:00:43 +0800

    this is updated

--- BookLatan/src/Model/BookDAO.java
+++ BookLatan/src/Model/BookDAO.java
@@ -3,7 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Model;
-
+/**
+ *
+ * @author Dinel
+ */
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
